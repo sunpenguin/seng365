74043fc3fde19b3d4f7957da8b85f00ec835ce02
Sunguin Peng <spe76@uclive.ac.nz>
2017-10-13 15:59:06 +1300

    Users can now log in and log out of the program.
    TODO
    Need to find a way to move variables around Vue files.

--- crowdfunding-app/src/CreateUser.vue
+++ crowdfunding-app/src/CreateUser.vue
@@ -10,19 +10,24 @@
             <p>AND HIS NAME IS...</p>
             <h1>JOHN CENA!</h1>
             <br />
-            <input type="text" v-model="username" placeholder="Username">
+            <input type="text" v-model="newUsername" placeholder="Username">
             {{ username }}
             <br />
-            <input type="password" v-model="password" placeholder="Password">
+            <input type="password" v-model="newPassword" placeholder="Password">
             {{ password }}
             <br />
-            <input type="email" v-model="email" placeholder="Email Address">
+            <input type="email" v-model="newEmail" placeholder="Email Address">
             {{ email }}
             <br />
-            <input type="text" v-model="location" placeholder="(optional)">
+            <input type="text" v-model="newLocation" placeholder="(optional)">
             {{ location }}
             <br />
             <button @click="createNewUser()">Create New Account</button>
+            <button @click="logIn()">Log In</button>
+            <button @click="logOut()">Log Out</button>
+            <br />
+            <p>{{ userId }}</p>
+            <p>{{ currentAuthenticationToken }}</p>
         </div>
     </div>
 </template>
@@ -33,48 +38,86 @@
             return {
                 error: "",
                 errorFlag: false,
-                newUser: {
-                    username: "",
-                    email: "",
-                    password: "",
-                    location: ""
-                },
-                user: ""
+
+                newUsername: "",
+                newEmail: "",
+                newPassword: "",
+                newLocation: "",
+
+                userId: "",
+                currentAuthenticationToken: ""
 
             }
         },
-        mounted: function() {
+        mounted: function () {
         },
         methods: {
-            createNewUser: function(){
-//                this.error = "Error with Account Creation:";
-//                this.errorFlag = false;
-//                if(!this.username){
-//                    this.error = this.error + "\n\n Please enter a username.";
-//                    this.errorFlag = true;
-//                }
-//                if(!this.password){
-//                    this.error = this.error + "\n\n Please enter a password.";
-//                    this.errorFlag = true;
-//                }
-//                let emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-//                if(!this.email || !emailRegex.test(this.email)){
-//                    this.error = this.error + "\n\n Please enter a valid email address.";
-//                    this.errorFlag = true;
-//                }
-                this.$http.post("http://localhost:4941/api/v2/users", this.newUser, {
+            createNewUser: function () {
+                this.error = "Error with Account Creation:";
+                this.errorFlag = false;
+
+                if (!this.newUsername) {
+                    this.error = this.error + "\n\n Please enter a username.";
+                    this.errorFlag = true;
+                    return;
+                }
+                if (!this.newPassword) {
+                    this.error = this.error + "\n\n Please enter a password.";
+                    this.errorFlag = true;
+                    return;
+                }
+                let emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+                if (!this.newEmail || !emailRegex.test(this.newEmail)) {
+                    this.error = this.error + "\n\n Please enter a valid email address.";
+                    this.errorFlag = true;
+                    return;
+                }
+
+                this.$http.post("http://localhost:4941/api/v2/users", {
+                    username: this.newUsername,
+                    email: this.newEmail,
+                    password: this.newPassword,
+                    location: this.newLocation
+                }, {
                     headers: {
                         'Content-Type': 'application/json'
                     }
                 })
                     .then(function (response) {
-                        this.user = response.data;
+                        this.logIn();
                     }, function (error) {
-                        this.error = error;
+                        this.error = "There was an error creating your account. Try using a different username and/or email.";
                         this.errorFlag = true;
                     });
+            },
+            logIn: function(){
+                this.$http.post("http://localhost:4941/api/v2/users/login", {}, {
+                    params: {
+                        username: this.newUsername,
+                        password: this.newPassword
+                    }
+                }).then(function(response){
+                    this.userId = response.data.id;
+                    this.currentAuthenticationToken = response.data.token;
+                }, function(error) {
+                    this.error = "Error Logging In!";
+                    this.errorFlag = true;
+                });
+            },
+            logOut: function() {
+                this.errorFlag = false;
+                this.$http.post("http://localhost:4941/api/v2/users/logout", {}, {
+                    headers: {
+                        'X-Authorization': this.currentAuthenticationToken
+                    }
+                }).then(function(response){
+                    this.userId = "";
+                    this.currentAuthenticationToken = "";
+                }, function(error) {
+                    this.error = "Error Logging Out!";
+                    this.errorFlag = true;
+                });
             }
-
         }
     }
 </script>
