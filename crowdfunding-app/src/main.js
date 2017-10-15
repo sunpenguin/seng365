17c6c3a150e3b1f64e2652c04f2c519308044d3c
Sunguin Peng <spe76@uclive.ac.nz>
2017-10-15 15:47:22 +1300

    Begin refining project details Begin implementing pledges CANNOT GET IT WORKING
    TODO
    - Make separate messages
    - Pledges
    - Create and manage projects
    - Search Filter
    - Get vue Bootstramp to work...

--- crowdfunding-app/src/main.js
+++ crowdfunding-app/src/main.js
@@ -18,6 +18,8 @@ Vue.use(VueX);
 
 import BootstrapVue from 'bootstrap-vue';
 Vue.use(BootstrapVue);
+// import 'bootstrap/dist/css/bootstrap.css'
+// import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 
 const routes = [
